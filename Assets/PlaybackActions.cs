7d36ce2382b174c5c8b20cf04cff131fb68733ce
John Austin <kleptine@gmail.com>
2017-04-02 16:29:43 -0700

    Working on sequencing.

--- Assets/PlaybackActions.cs
+++ Assets/PlaybackActions.cs
@@ -11,15 +11,18 @@ public class PlaybackActions : MonoBehaviour {
 	public void Update () {
 	  if (recording.Count > 0) {
       // start time is the end of the last loop
-	    double recording_start_time = recording.First().timestamp;
-	    double playback_start_time = recording.Last().timestamp;
+	    double sequence_period = 5.0;
+	    double sequence_time = Time.realtimeSinceStartup % sequence_period;
 	    double total_recording_length = recording.Last().timestamp - recording.First().timestamp;
-	    double playback_time = (Time.realtimeSinceStartup - playback_start_time) % total_recording_length;
+
+	    double recording_start_time = recording.First().timestamp % sequence_period;
+	    double playback_start_time = recording.Last().timestamp % sequence_period;
+	    double playback_time = (sequence_time - playback_start_time) % total_recording_length;
 	    double playback_time_original = playback_time + recording_start_time;
 
 	    playback_cursor = recording[0];
       foreach (RecordActions.Snapshot snap in recording) {
-        if (snap.timestamp > playback_time_original) {
+        if (snap.timestamp % sequence_period > playback_time_original % sequence_period) {
           break;
         }
         playback_cursor = snap;
@@ -28,7 +31,6 @@ public class PlaybackActions : MonoBehaviour {
 			// update to match recording
 	    transform.position = playback_cursor.position;
 	    transform.rotation = playback_cursor.rotation;
-
 	  }
  	}
 }
