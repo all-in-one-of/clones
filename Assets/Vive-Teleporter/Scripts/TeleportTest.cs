c8a2b826c0ade36ac04eafc4d0446ad0174aad27
John Austin <kleptine@gmail.com>
2017-05-09 17:08:04 -0700

    Adds vive-like teleporting. Use Trigger.

---
+++ Assets/Vive-Teleporter/Scripts/TeleportTest.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+/// \brief A test script for "The Lab" style teleportation if you don't have a Vive.  Keep in mind that this 
+/// doesn't have fade in/out, whereas TeleportVive (a version of this specifically made for the Vive) does.
+/// \sa TeleportVive
+[AddComponentMenu("Vive Teleporter/Test/Teleporter Test (No SteamVR)")]
+public class TeleportTest : MonoBehaviour {
+  public Transform Controller;
+
+  public GUIStyle InstructionsStyle;
+
+  public Camera LookCamera;
+  public float LookSensitivity = 10;
+
+  public float MovementSpeed = 1;
+  public ParabolicPointer Pointer;
+  public float PointSensitivity = 10;
+
+  private float pointer_pitch;
+  private float pointer_yaw;
+
+  private void Update() {
+    Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * MovementSpeed;
+    move = transform.TransformDirection(move);
+    transform.Translate(move * Time.deltaTime);
+
+    if (Input.GetButton("Switch Control")) {
+      transform.Rotate(0, Input.GetAxis("Mouse X") * LookSensitivity, 0);
+      LookCamera.transform.Rotate(-Input.GetAxis("Mouse Y") * LookSensitivity, 0, 0);
+    } else {
+      pointer_pitch += -Input.GetAxis("Mouse Y") * PointSensitivity;
+      pointer_yaw += Input.GetAxis("Mouse X") * PointSensitivity;
+      Controller.localRotation = Quaternion.Euler(pointer_pitch, pointer_yaw, 0);
+    }
+
+    if (Input.GetButtonDown("Click") && Pointer.PointOnNavMesh) transform.position = Pointer.SelectedPoint;
+  }
+
+  private void OnGUI() {
+    GUI.Label(new Rect(Screen.width / 2 - 150, Screen.height - 60, 300, 50),
+      "Hold ALT to turn camera, Click to teleport, Mouse to rotate controller/camera", InstructionsStyle);
+  }
+}
---
+++ Assets/Vive-Teleporter/Scripts/ViveNavMesh.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Rendering;
+using UnityEngine.Serialization;
+
+/// \brief A version of Unity's baked navmesh that is converted to a (serializable) component.  This allows the navmesh 
+/// used for Vive navigation to be separated form the AI Navmesh.  ViveNavMesh also handles the rendering of the 
+/// NavMesh grid in-game.
+[AddComponentMenu("Vive Teleporter/Vive Nav Mesh")]
+[RequireComponent(typeof(BorderRenderer))]
+[ExecuteInEditMode]
+public class ViveNavMesh : MonoBehaviour {
+  [SerializeField]
+  private readonly NavmeshDewarpingMethod _DewarpingMethod = NavmeshDewarpingMethod.None;
+
+  [SerializeField]
+  private readonly bool _IgnoreSlopedSurfaces = true;
+
+  [SerializeField]
+  [HideInInspector]
+  private readonly int _NavAreaMask = ~0; // Initialize to all
+
+  [SerializeField]
+  private readonly float _SampleRadius = 0.25f;
+
+  private readonly Dictionary<Camera, CommandBuffer> cameras = new Dictionary<Camera, CommandBuffer>();
+
+  /// \brief The alpha (transparency) value of the rendered ground mesh)
+  /// \sa GroundMaterial
+  [Range(0, 1)]
+  public float GroundAlpha = 1.0f;
+
+  private Material _GroundMaterial;
+
+  [FormerlySerializedAs("_GroundMaterial")]
+  [SerializeField]
+  private Material _GroundMaterialSource;
+
+  [SerializeField]
+  private bool _IgnoreLayerMask = true;
+
+  [SerializeField]
+  private int _LayerMask;
+
+  [SerializeField]
+  private int _QueryTriggerInteraction;
+
+  [SerializeField]
+  [HideInInspector]
+  private Mesh _SelectableMesh;
+
+  [SerializeField]
+  [HideInInspector]
+  private BorderPointSet[] _SelectableMeshBorder;
+
+  private int AlphaShaderID = -1;
+
+  private BorderRenderer Border;
+  private float LastGroundAlpha = 1.0f;
+
+  /// Material used for the floor mesh when the user is selecting a point to teleport to
+  public Material GroundMaterial {
+    get { return _GroundMaterial; }
+    set {
+      Material old = _GroundMaterial;
+      _GroundMaterial = value;
+      if (_GroundMaterial != null) _GroundMaterial.SetFloat(AlphaShaderID, GroundAlpha);
+      if (old != _GroundMaterial) Cleanup();
+    }
+  }
+
+  public int LayerMask {
+    get { return _LayerMask; }
+    set { _LayerMask = value; }
+  }
+
+  public bool IgnoreLayerMask {
+    get { return _IgnoreLayerMask; }
+    set { _IgnoreLayerMask = value; }
+  }
+
+  public int QueryTriggerInteraction {
+    get { return _QueryTriggerInteraction; }
+    set { _QueryTriggerInteraction = value; }
+  }
+
+  /// A Mesh that represents the "Selectable" area of the world.  This is converted from Unity's NavMesh in ViveNavMeshEditor
+  public Mesh SelectableMesh {
+    get { return _SelectableMesh; }
+    set {
+      _SelectableMesh = value;
+      Cleanup();
+    } // Cleanup because we need to change the mesh inside command buffers
+  }
+
+  /// \brief The border points of SelectableMesh.  This is automatically generated in ViveNavMeshEditor.
+  /// This is an array of Vector3 arrays, where each Vector3 array is the points in a polyline.  These polylines combined
+  /// describe the borders of SelectableMesh.  We have to use BorderPointSets instead of a jagged Vector3[][] array because
+  /// Unity can't serialize jagged arrays for some reason.
+  public BorderPointSet[] SelectableMeshBorder {
+    get { return _SelectableMeshBorder; }
+    set {
+      _SelectableMeshBorder = value;
+      Border.Points = _SelectableMeshBorder;
+    }
+  }
+
+  public bool IgnoreSlopedSurfaces {
+    get { return _IgnoreSlopedSurfaces; }
+  }
+
+  public float SampleRadius {
+    get { return _SampleRadius; }
+  }
+
+  public NavmeshDewarpingMethod DewarpingMethod {
+    get { return _DewarpingMethod; }
+  }
+
+  private void Start() {
+    if (SelectableMesh == null) SelectableMesh = new Mesh();
+    if (_SelectableMeshBorder == null) _SelectableMeshBorder = new BorderPointSet[0];
+
+    Border = GetComponent<BorderRenderer>();
+    Border.Points = SelectableMeshBorder;
+
+    AlphaShaderID = Shader.PropertyToID("_Alpha");
+
+    if (_GroundMaterialSource != null) GroundMaterial = new Material(_GroundMaterialSource);
+
+    if (GroundAlpha != LastGroundAlpha && GroundMaterial != null) {
+      GroundMaterial.SetFloat(AlphaShaderID, GroundAlpha);
+      LastGroundAlpha = GroundAlpha;
+    }
+
+#if UNITY_EDITOR
+    SceneView.RepaintAll();
+#endif
+  }
+
+  private void Update() {
+    // We have to detect changes this way instead of using properties because
+    // we want to be able to animate the alpha value with a Unity animator.
+    if (GroundAlpha != LastGroundAlpha && GroundMaterial != null) {
+      GroundMaterial.SetFloat(AlphaShaderID, GroundAlpha);
+      LastGroundAlpha = GroundAlpha;
+    }
+  }
+
+  private void Cleanup() {
+    foreach (var cam in cameras) {
+      if (cam.Key) {
+        cam.Key.RemoveCommandBuffer(CameraEvent.AfterForwardOpaque, cam.Value);
+      }
+    }
+    cameras.Clear();
+  }
+
+  public void OnEnable() {
+    Cleanup();
+  }
+
+  public void OnDisable() {
+    Cleanup();
+  }
+
+  private void OnRenderObject() {
+    // We have to use command buffers instead of Graphics.DrawMesh because of strange depth issues that I am experiencing
+    // with Graphics.Drawmesh (perhaps Graphics.DrawMesh is called before all opaque objects are rendered?)
+    var act = gameObject.activeInHierarchy && enabled;
+    if (!act) {
+      Cleanup();
+      return;
+    }
+
+    // If _SelectableMesh == null there is a crash in Unity 5.4 beta (apparently you can't pass null to CommandBuffer::DrawMesh now).
+    if (!_SelectableMesh || !GroundMaterial) return;
+
+    var cam = Camera.current;
+    if (!cam || cam.cameraType == CameraType.Preview || ((1 << gameObject.layer) & Camera.current.cullingMask) == 0) return;
+
+    CommandBuffer buf = null;
+    if (cameras.ContainsKey(cam)) return;
+
+    buf = new CommandBuffer();
+    // Note: Mesh is drawn slightly pushed upwards to avoid z-fighting issues
+    buf.DrawMesh(_SelectableMesh, Matrix4x4.TRS(Vector3.up * 0.005f, Quaternion.identity, Vector3.one), GroundMaterial,
+      0);
+    cameras[cam] = buf;
+    cam.AddCommandBuffer(CameraEvent.AfterForwardOpaque, buf);
+  }
+
+  private void OnValidate() {
+    Border = GetComponent<BorderRenderer>();
+    Border.Points = SelectableMeshBorder;
+
+    if (AlphaShaderID == -1) AlphaShaderID = Shader.PropertyToID("_Alpha");
+  }
+
+  /// \brief Casts a ray against the Navmesh and attempts to calculate the ray's worldspace intersection with it.
+  /// This uses Physics raycasts to perform the raycast calculation, so the teleport surface must have a collider
+  /// on it.
+  /// \param p1 First (origin) point of ray
+  /// \param p2 Last (end) point of ray
+  /// \param pointOnNavmesh If the raycast hit something on the navmesh.
+  /// \param hitPoint If hit, the point of the hit.  Otherwise zero.
+  /// \param normal If hit, the normal of the hit surface.  Otherwise (0, 1, 0)
+  /// \return If the raycast hit something.
+  public bool Linecast(Vector3 p1, Vector3 p2, out bool pointOnNavmesh, out Vector3 hitPoint, out Vector3 normal) {
+    RaycastHit hit;
+    Vector3 dir = p2 - p1;
+    float dist = dir.magnitude;
+    dir /= dist;
+    if (Physics.Raycast(p1, dir, out hit, dist, _IgnoreLayerMask ? ~_LayerMask : _LayerMask,
+      (QueryTriggerInteraction) _QueryTriggerInteraction)) {
+      normal = hit.normal;
+      if (Vector3.Dot(Vector3.up, hit.normal) < 0.99f && _IgnoreSlopedSurfaces) {
+        pointOnNavmesh = false;
+        hitPoint = hit.point;
+
+        return true;
+      }
+
+      hitPoint = hit.point;
+      NavMeshHit navHit;
+      pointOnNavmesh = NavMesh.SamplePosition(hitPoint, out navHit, _SampleRadius, _NavAreaMask);
+
+      return true;
+    }
+    pointOnNavmesh = false;
+    hitPoint = Vector3.zero;
+    normal = Vector3.up;
+    return false;
+  }
+}
+
+[Serializable]
+public class BorderPointSet {
+  public Vector3[] Points;
+
+  public BorderPointSet(Vector3[] Points) {
+    this.Points = Points;
+  }
+}
+
+[Serializable]
+public enum NavmeshDewarpingMethod {
+  None,
+  RoundToVoxelSize,
+  RaycastDownward
+}
